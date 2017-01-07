b45f7ebb9750f7d8911a9c3b62004f7736a650e5
koriavinash <koriavinash1@gmail.com>
2017-01-08 02:29:43 +0530

    search by area

--- scripts/base.js
+++ scripts/base.js
@@ -1,4 +1,4 @@
-var general_app = angular.module('anveshak',[]);
+var general_app = angular.module('masterstroke',[]);
 general_app.config(['$httpProvider','$interpolateProvider',function($httpProvider,$interpolateProvider){
 	$httpProvider.defaults.useXDomain = true;
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
@@ -9,5 +9,5 @@ general_app.controller("base", function($scope,$http) {
 	$scope.flag=1;$scope.option=1;
 	console.log($scope.option);
 	$scope.height = window.innerHeight;
-	
+
 });
--- scripts/map.js
+++ scripts/map.js
@@ -7,9 +7,12 @@ general_app.config(['$httpProvider','$interpolateProvider',function($httpProvide
 }]);
 general_app.controller("map", function($scope,$http) {
 	$scope.height = window.innerHeight;
-	console.log($scope.height);
-	console.log($scope.area);
-	console.log($scope.city);
-	console.log($scope.country);
-
+	$scope.search = function(){
+		console.log($scope.height);
+		console.log($scope.area);
+		console.log($scope.city);
+		console.log($scope.country);
+		$scope.location = $scope.area.toString() + $scope.city.toString() + $scope.country.toString();
+		$('#pac-input').attr('value',location);
+	};
 });
