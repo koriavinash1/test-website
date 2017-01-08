7b51a244770cc0b8d84ec807cad587c59a00c93f
koriavinash <koriavinash1@gmail.com>
2017-01-08 10:53:29 +0530

    input testing

---
+++ scripts/location.js
@@ -0,0 +1,32 @@
+var general_app = angular.module('masterstroke',[]);
+general_app.config(['$httpProvider','$interpolateProvider',function($httpProvider,$interpolateProvider){
+	$httpProvider.defaults.useXDomain = true;
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    $interpolateProvider.startSymbol('{[{');
+    $interpolateProvider.endSymbol('}]}');
+}]);
+general_app.controller("map", function($scope,$http) {
+	$scope.height = window.innerHeight;
+	$scope.search = function(){
+		console.log($scope.height);
+		console.log($scope.area);
+		console.log($scope.city);
+		console.log($scope.country);
+		var location = $scope.area.toString() + $scope.city.toString() + $scope.country.toString();
+    setcookie("location", location, 15);
+    console.log("hello");
+	};
+});
+
+function setcookie(name, value, days)
+{
+  if (days)
+  {
+    var date = new Date();
+    date.setTime(date.getTime()+days*24*60*60*1000); // ) removed
+    var expires = "; expires=" + date.toGMTString(); // + added
+  }
+  else
+    var expires = "";
+  document.cookie = name+"=" + value + expires + ";path=/"; // + and " added
+}
--- scripts/map.js
+++ scripts/map.js
@@ -5,20 +5,7 @@ general_app.config(['$httpProvider','$interpolateProvider',function($httpProvide
     $interpolateProvider.startSymbol('{[{');
     $interpolateProvider.endSymbol('}]}');
 }]);
-var location = "";
-general_app.controller("map", function($scope,$http) {
-	$scope.height = window.innerHeight;
-	$scope.search = function(){
-		console.log($scope.height);
-		console.log($scope.area);
-		console.log($scope.city);
-		console.log($scope.country);
-		location = $scope.area.toString() + $scope.city.toString() + $scope.country.toString();
-
-	};
-});
 
 general_app.controller("maplocation",function($scope,$http){
 	$('#pac-input').attr('value',location);
-	console.log(location);
 });
